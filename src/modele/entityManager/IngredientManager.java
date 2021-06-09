2234a1098106afafb35303ce6d66d8e31f24c1d5
PERRIN Baptiste <baptisteperrin77@gmail.com>
2021-06-09 11:21:57 +0200

    Ajout IngredientManager

---
+++ src/modele/entityManager/IngredientManager.java
@@ -0,0 +1,110 @@
+package modele.entityManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.entity.Adresse;
+import modele.entity.Client;
+import modele.entity.Ingredient;
+
+public class IngredientManager implements EntityManager<Ingredient> {
+
+	private static String TABLE_NAME = "Ingredient";
+	private static String ID_COLUMN = "id_ingredient";
+
+	private Connection connection;
+
+	public IngredientManager(Connection c) {
+		this.connection = c;
+	}
+
+	@Override
+	public Ingredient processLine(ResultSet rs) {
+		Ingredient ingredient = new Ingredient();
+		// TODO: Ajouter les pizza utilisant les ingredients
+
+		try {
+			ingredient.setIdIngredient(rs.getInt(ID_COLUMN));
+			ingredient.setNomIngredient(rs.getString("nom_ingredient"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return ingredient;
+	}
+
+	@Override
+	public List<Ingredient> getAll() {
+		List<Ingredient> ingredients = new ArrayList<>();
+
+		ResultSet results;
+
+		try {
+			Statement stmt = connection.createStatement();
+			results = stmt.executeQuery("SELECT * FROM " + TABLE_NAME + ";");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		try {
+			while (results.next()) {
+				ingredients.add(processLine(results));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return ingredients;
+	}
+
+	@Override
+	public Ingredient getOneById(int id) {
+		ResultSet results;
+
+		try {
+			Statement stmt = connection.createStatement();
+			results = stmt.executeQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + ID_COLUMN + "=" + id + ";");
+			results.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return processLine(results);
+	}
+
+	@Override
+	public void deleteOneById(int id) {
+		try {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("DELETE FROM " + TABLE_NAME + " WHERE " + ID_COLUMN + "=" + id + ";");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void updateOne(Ingredient entity) {
+		try {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("UPDATE " + TABLE_NAME + " SET " 
+			+ "nom_ingredient ='" + entity.getNomIngredient()
+			+ "' WHERE " + ID_COLUMN + "=" + entity.getIdIngredient()
+			+ ";");
+			
+			//TODO: Update les ingredients en fonction de la liste
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
