156ff20bb466162aa7695bda8418be5dd75a2600
PERRIN Baptiste <baptisteperrin77@gmail.com>
2021-06-16 11:38:07 +0200

    Ajout d'un Singleton pour la transmission de données entre les scenes

---
+++ src/controller/ApplicationManager.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.sql.Connection;
+
+public final class ApplicationManager {
+	
+	
+	/**
+	 * Gestion du singleton
+	 */
+	private ApplicationManager() {}
+	
+	private static ApplicationManager INSTANCE = null;
+	
+	public ApplicationManager getInstance() {
+		if(INSTANCE == null) INSTANCE = new ApplicationManager();		
+		return INSTANCE;
+	}
+	
+	/**
+	 * Données du singleton
+	 */
+	private Connection databaseConnection;
+
+	public Connection getDatabaseConnection() {
+		return databaseConnection;
+	}
+
+	public void setDatabaseConnection(Connection databaseConnection) {
+		this.databaseConnection = databaseConnection;
+	}
+	
+
+}
