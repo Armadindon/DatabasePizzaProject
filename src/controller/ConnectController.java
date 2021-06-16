dd876c365626412938247f550e32730900a5a955
pellanm <mateo.pellan@gmail.com>
2021-06-16 11:32:32 +0200

    correction pépin

--- src/controller/ConnectController.java
+++ src/controller/ConnectController.java
@@ -44,28 +44,22 @@ public class ConnectController {
     void validateConnection(ActionEvent event) {
     	databaseConnection("org.mariadb.jdbc.Driver", tf_bdd.getText(), tf_password.getText(), tf_login.getText());
     
-    	//sendData(event);
+    	sendData(event);
     }
     
     @FXML
     private void sendData(ActionEvent event) {
-	      // Step 2
 	      Node node = (Node) event.getSource();
-	      // Step 3
 	      Stage stage = (Stage) node.getScene().getWindow();
 	      stage.close();
 	      
 	      try {
-		        // Step 4
-		        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Menu.fxml"));
-		        // Step 5
+		        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/Menu.fxml"));
+		        
 		        stage.setUserData(connection);
-		        // Step 6
 		        Scene scene = new Scene(root);
 		        stage.setScene(scene);
-		        // Step 7
 		        stage.show();
-		        
 	      } catch (IOException e) {
 	    	  System.err.println(String.format("Error: %s", e.getMessage()));
 	      }
