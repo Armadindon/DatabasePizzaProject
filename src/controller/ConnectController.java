042142851395e862fabee6cd1ef900b0cd740884
PERRIN Baptiste <baptisteperrin77@gmail.com>
2021-06-16 10:48:17 +0200

    connection en utilisant les champs

--- src/controller/ConnectController.java
+++ src/controller/ConnectController.java
@@ -37,12 +37,12 @@ public class ConnectController {
     
 
     /***
-     * Fonction affiliée au bouton de connexion
+     * Fonction affiliï¿½e au bouton de connexion
      * @param event
      */
     @FXML
     void validateConnection(ActionEvent event) {
-    	System.out.println(tf_login.getText() + " " + tf_password.getText());
+    	databaseConnection("com.mysql.cj.jdbc.Driver", tf_bdd.getText(), tf_password.getText(), tf_login.getText());
     }
     
     public void databaseConnection(String driver, String url, String user, String pwd) {
